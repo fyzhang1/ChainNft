8f1a6df39c19bac5212975a2ffab50be622a8245
zfy <1093554702@qq.com>
2022-09-07 10:41:54 +0800

    Initial commit

---
+++ src/test/java/com/example/chainnft/ChainNftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chainnft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainNftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
